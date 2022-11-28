c7548dc686604b3e6f67524a26cdc327563f18ef
timo282 <ti-heiss@t-online.de>
2022-11-28 21:15:35 +0100

    Adds preparation for yolov5

---
+++ preparation_for_yolov5.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from tqdm import tqdm
+
+data = pd.read_csv(r'generated_data\data3\labels\generated_graph_legends.csv')
+path = r'generated_data\data3\labels'
+
+for _, row in tqdm(data.iterrows()):
+    x_center_n = (row['xmin']+row['width']/2) / 1024
+    y_center_n = (row['ymin']+row['height']/2) / 512
+    width_n = row['width'] / 1024
+    height_n = row['height'] / 512
+    s = f"0 {x_center_n} {y_center_n} {width_n} {height_n}"
+    with open(path + "\\" + row['sample_name'].replace("png", "txt"), 'w') as f:
+        f.write(s)
