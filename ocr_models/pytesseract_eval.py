0e26dea0002ca05a5011bd88f6e4e54403609422
timo282 <ti-heiss@t-online.de>
2022-11-29 22:38:08 +0100

    Adds model and file for first submission

--- ocr_models/pytesseract_eval.py
+++ ocr_models/pytesseract_eval.py
@@ -1,12 +1,12 @@
-from eval_interface import EvalInterface
-from preprocessing.preprocessor import Preprocessor
+# from eval_interface import EvalInterface
+# from preprocessing.preprocessor import Preprocessor
 import pytesseract
 from pytesseract import Output
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-class PytesseractEval(EvalInterface):
+class PytesseractEval():
     def __init__(self, 
-                preprocessor:Preprocessor=Preprocessor(), 
+                preprocessor=None, 
                 custom_config=None, confidence=50):
         if custom_config == None:
             self.custom_config = r'-l eng+grc -c tessedit_char_blacklist=0123456789 --psm 11'
@@ -17,7 +17,7 @@ class PytesseractEval(EvalInterface):
         self.name = f"pytesseract_{self.custom_config}_{self.confidence}_{self.preprocessor.name}"
 
     def predict(self, img):
-        img = img[0]
+        # img = img[0]
         img = self.preprocessor.preprocess(img)
         result = []
         d = pytesseract.image_to_data(img, output_type=Output.DICT, config=self.custom_config)
@@ -28,7 +28,7 @@ class PytesseractEval(EvalInterface):
 
         res = [ee for e in result for ee in e.strip().split("\n")]
 
-        return [list(filter(("").__ne__, [e.strip().replace("\n", "") for e in res]))]
+        return list(filter(("").__ne__, [e.strip().replace("\n", "") for e in res]))
 
     def get_name(self):
         return self.name
